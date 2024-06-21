31f88f5b81b87553ad556cc1d0237458ce525c9e
JHyeok-Choi <wogur8546@gmail.com>
2024-06-21 09:40:59 +0900

    streamlit file path test

--- model/model.py
+++ model/model.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import LabelEncoder
 from keras.models import load_model
 
 
-model = load_model('data/urban_sound_model.h5')
+model = load_model('urban_sound_model.h5')
 
 def extract_feature(file_name):
 
